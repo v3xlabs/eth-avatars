9a8c828b7598b38949526e4f3f52f532ce2640df
Jonatan Holmgren <jonatan@jontes.page>
2026-08-28 16:24:22 +0200

    Support multiple NFT image metadata formats and handle inline SVG data

--- cargo/src/modules/ethereum/nft.rs
+++ cargo/src/modules/ethereum/nft.rs
@@ -5,6 +5,8 @@ use crate::{FetchError, Resource, resource::Decoder};
 #[derive(Deserialize)]
 pub struct NftMetadata {
     pub image: Option<String>,
+    pub image_url: Option<String>,
+    pub image_data: Option<String>,
 }
 
 pub struct NftMetadataDecoder;
@@ -13,7 +15,19 @@ impl Decoder for NftMetadataDecoder {
     fn decode(&self, bytes: Vec<u8>) -> Result<Resource, FetchError> {
         serde_json::from_slice::<NftMetadata>(&bytes)
             .map_err(|_| FetchError::Unsupported)
-            .and_then(|x| x.image.ok_or(FetchError::Unsupported))
-            .and_then(|x| x.parse().map_err(Into::into))
+            .and_then(|metadata| {
+                metadata
+                    .image
+                    .or(metadata.image_url)
+                    .or(metadata.image_data)
+                    .ok_or(FetchError::Unsupported)
+            })
+            .and_then(|image| {
+                if image.starts_with('<') {
+                    Ok(Resource::Raw(image.into_bytes()))
+                } else {
+                    image.parse().map_err(Into::into)
+                }
+            })
     }
 }
